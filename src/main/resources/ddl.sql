5ad4250c47b0abe2e7536c43447daf448dc73005
Carlos Eduardo C. da Silva <carloseduardoweb@yahoo.com.br>
2019-09-03 18:13:55 -0300

    Atualizadas algumas configurações do projeto.

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,9 +1,22 @@
+--SQL Server
+--create database springboot;
+--
+--use springboot;
+--
+--create table tb_produtos (
+--	id int identity(1,1) primary key,
+--	nome varchar(30) not null,
+--	descricao varchar(150)
+--);
+
+-- MySQL
 create database springboot;
 
 use springboot;
 
 create table tb_produtos (
-	id int identity(1,1) primary key,
+	id int primary key auto_increment,
 	nome varchar(30) not null,
 	descricao varchar(150)
 );
+
